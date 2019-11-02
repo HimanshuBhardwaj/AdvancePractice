2fa9fc371b557fea5c93d625aac7adf8740ac8a2
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2019-11-02 20:34:51 +0530

    First topcoder

--- src/main/java/com/himanshu/practice/nov/nov1/D.java
+++ src/main/java/com/himanshu/practice/nov/nov1/D.java
@@ -3,6 +3,7 @@ package com.himanshu.practice.nov.nov1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 /**
  * Created by himanshubhardwaj on 01/11/19.
@@ -10,6 +11,37 @@ import java.io.InputStreamReader;
 public class D {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
+        int t = Integer.parseInt(br.readLine());
+        PrintWriter pw = new PrintWriter(System.out);
+
+
+        while (t-- > 0) {
+            String[] str = br.readLine().split(" ");
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+
+            if (gcd(a, b) == 1) {
+                pw.append("Finite\n");
+            } else {
+                pw.append("Infinite\n");
+            }
+
+        }
+        pw.flush();
+        pw.close();
+    }
+
+    static int gcd(int a, int b) {
+        if (a < b) {
+            return gcd(b, a);
+        }
+        if (b == 0) {
+            return a;
+        }
+        if (b == 1) {
+            return 1;
+        }
+
+        return gcd(a % b, b);
     }
 }
